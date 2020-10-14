f6ae13f03b36e96c3acfa1a6d0e43791753d3891
Yuriy Lu <youlu@outlook.com>
2020-10-14 12:39:50 +0800

    Update: add external error code support

--- errors/errors.go
+++ errors/errors.go
@@ -3,6 +3,7 @@ package errors
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/appootb/protobuf/go/code"
 	spb "google.golang.org/genproto/googleapis/rpc/status"
@@ -11,12 +12,16 @@ import (
 
 type StatusError spb.Status
 
-func CodeType(err error) code.Error {
+func ErrorCode(err error) int32 {
 	var stErr *StatusError
 	if errors.As(err, &stErr) {
-		return code.Error(stErr.Code)
+		return stErr.Code
 	}
-	return code.Error(codes.Unknown)
+	return int32(codes.Unknown)
+}
+
+func CodeType(err error) code.Error {
+	return code.Error(ErrorCode(err))
 }
 
 func (err *StatusError) Error() string {
@@ -24,16 +29,31 @@ func (err *StatusError) Error() string {
 	return fmt.Sprintf("status error: code = %s desc = %s", codes.Code(p.GetCode()), p.GetMessage())
 }
 
-func New(code code.Error, msg string) error {
+func parseCode(c interface{}) int32 {
+	switch v := c.(type) {
+	case int32:
+		return v
+	case code.Error:
+		return int32(v)
+	default:
+		i, err := strconv.Atoi(fmt.Sprintf("%d", v))
+		if err != nil {
+			panic("unknown error code, err: " + err.Error())
+		}
+		return int32(i)
+	}
+}
+
+func New(code interface{}, msg string) error {
 	return &StatusError{
-		Code:    int32(code),
+		Code:    parseCode(code),
 		Message: msg,
 	}
 }
 
-func Newf(code code.Error, format string, a ...interface{}) error {
+func Newf(code interface{}, format string, a ...interface{}) error {
 	return &StatusError{
-		Code:    int32(code),
+		Code:    parseCode(code),
 		Message: fmt.Sprintf(format, a...),
 	}
 }
