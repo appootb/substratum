d7022ca63f2fc58e14a56369e1c023fd01567f21
Yuriy Lu <youlu@outlook.com>
2020-10-29 20:21:08 +0800

    Update valuepb

--- util/valuepb/value.go
+++ util/valuepb/value.go
@@ -4,19 +4,63 @@ import (
 	"fmt"
 	"strconv"
 
-	structpb "github.com/golang/protobuf/ptypes/struct"
+	"google.golang.org/protobuf/types/known/structpb"
 )
+
+func BoolValue(b bool) *structpb.Value {
+	return &structpb.Value{
+		Kind: &structpb.Value_BoolValue{
+			BoolValue: b,
+		},
+	}
+}
 
 func Bool(v *structpb.Value) bool {
 	return int64(Float64(v)) != 0
+}
+
+func Int64Value(v int64) *structpb.Value {
+	return &structpb.Value{
+		Kind: &structpb.Value_NumberValue{
+			NumberValue: float64(v),
+		},
+	}
 }
 
 func Int64(v *structpb.Value) int64 {
 	return int64(Float64(v))
 }
 
+func Uint64Value(v uint64) *structpb.Value {
+	return &structpb.Value{
+		Kind: &structpb.Value_NumberValue{
+			NumberValue: float64(v),
+		},
+	}
+}
+
+func Uint64(v *structpb.Value) uint64 {
+	return uint64(Float64(v))
+}
+
+func Int32Value(v int32) *structpb.Value {
+	return &structpb.Value{
+		Kind: &structpb.Value_NumberValue{
+			NumberValue: float64(v),
+		},
+	}
+}
+
 func Int32(v *structpb.Value) int32 {
 	return int32(Float64(v))
+}
+
+func Float64Value(v float64) *structpb.Value {
+	return &structpb.Value{
+		Kind: &structpb.Value_NumberValue{
+			NumberValue: v,
+		},
+	}
 }
 
 func Float64(v *structpb.Value) float64 {
@@ -32,6 +76,14 @@ func Float64(v *structpb.Value) float64 {
 		}
 	}
 	return 0
+}
+
+func StringValue(s string) *structpb.Value {
+	return &structpb.Value{
+		Kind: &structpb.Value_StringValue{
+			StringValue: s,
+		},
+	}
 }
 
 func String(v *structpb.Value) string {
