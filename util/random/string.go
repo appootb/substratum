e4e5cf2a8c4a93d78b1435e578900dfd459dd5ec
Yuriy Lu <youlu@outlook.com>
2021-05-29 13:02:07 +0800

    Fix rand panic issue

--- util/random/string.go
+++ util/random/string.go
@@ -13,19 +13,18 @@ const (
 	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits.
 )
 
-var (
-	src = rand.NewSource(time.Now().UnixNano())
-	rnd = rand.New(src)
-)
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 // Generate random string using masking with source.
 func String(n int) string {
 	b := make([]byte, n)
 	l := len(letterBytes)
 	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
+			cache, remain = rand.Int63(), letterIdxMax
 		}
 		if idx := int(cache & letterIdxMask); idx < l {
 			b[i] = letterBytes[idx]
