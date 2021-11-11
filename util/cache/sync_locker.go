c299391e6ed54502232d786fd5e79fb7c54224ea
Yuriy Lu <youlu@outlook.com>
2021-11-11 16:07:08 +0800

    Fix: set lru repeatly when GetOrLoad

--- util/cache/sync_locker.go
+++ util/cache/sync_locker.go
@@ -2,11 +2,13 @@ package cache
 
 import (
 	"sync"
+	"time"
 )
 
 type caller struct {
 	ch  chan bool
 	val interface{}
+	dur time.Duration
 	err error
 }
 
@@ -17,7 +19,7 @@ type syncLocker struct {
 	m  map[interface{}]*caller
 }
 
-func (sg *syncLocker) Invoke(key interface{}, fn func() (interface{}, error)) (interface{}, error) {
+func (sg *syncLocker) Invoke(key interface{}, fn LoaderFunc) (interface{}, error) {
 	sg.mu.Lock()
 	if v, ok := sg.cache.Peek(key); ok {
 		sg.mu.Unlock()
@@ -37,7 +39,8 @@ func (sg *syncLocker) Invoke(key interface{}, fn func() (interface{}, error)) (i
 	sg.mu.Unlock()
 
 	// do invoke, and close caller channel
-	c.val, c.err = fn()
+	c.val, c.dur, c.err = fn(key)
+	sg.cache.Set(key, c.val, c.dur)
 	close(c.ch)
 	// remove from caller map
 	sg.mu.Lock()
