8b834239d3e57e0d43be9e5106fa7b30bffdad1f
Yuriy Lu <youlu@outlook.com>
2021-11-18 15:10:43 +0800

    Update: consumer pool

--- util/pool/consumer_pool_test.go
+++ util/pool/consumer_pool_test.go
@@ -8,15 +8,13 @@ import (
 
 func TestConsumerPool_Merge(t *testing.T) {
 	consumed := make(chan int, 10)
-	pool := NewConsumer(1, ConsumerFunc(func(ctx context.Context, arg map[interface{}][]interface{}) {
-		for _, v := range arg {
-			consumed <- len(v)
-		}
+	pool := NewConsumer(1, ConsumerFunc(func(ctx context.Context, k interface{}, v []interface{}) {
+		consumed <- len(v)
 	}))
 	size := 10
 	start := time.Now()
 	for i := 0; i < size; i++ {
-		if ok := pool.Add(i, i); !ok {
+		if ok := pool.Add("TestConsumerPool_Merge", i); !ok {
 			t.Error("add data failed")
 		}
 	}
@@ -34,15 +32,13 @@ func TestConsumerPool_Merge(t *testing.T) {
 
 func TestConsumerPool_Merge2(t *testing.T) {
 	consumed := make(chan int, 10)
-	pool := NewConsumer(1, ConsumerFunc(func(ctx context.Context, arg map[interface{}][]interface{}) {
-		for _, v := range arg {
-			consumed <- len(v)
-		}
+	pool := NewConsumer(1, ConsumerFunc(func(ctx context.Context, k interface{}, v []interface{}) {
+		consumed <- len(v)
 	}))
 	size := 20
 	start := time.Now()
 	for i := 0; i < size; i++ {
-		if ok := pool.Add(i, i); !ok {
+		if ok := pool.Add("TestConsumerPool_Merge2", i); !ok {
 			t.Error("add data failed")
 		}
 	}
@@ -60,15 +56,13 @@ func TestConsumerPool_Merge2(t *testing.T) {
 
 func TestConsumerPool_Duration(t *testing.T) {
 	consumed := make(chan int, 10)
-	pool := NewConsumer(1, ConsumerFunc(func(ctx context.Context, arg map[interface{}][]interface{}) {
-		for _, v := range arg {
-			consumed <- len(v)
-		}
+	pool := NewConsumer(1, ConsumerFunc(func(ctx context.Context, k interface{}, v []interface{}) {
+		consumed <- len(v)
 	}))
 	size := 9
 	start := time.Now()
 	for i := 0; i < size; i++ {
-		if ok := pool.Add(i, i); !ok {
+		if ok := pool.Add("TestConsumerPool_Duration", i); !ok {
 			t.Error("add data failed")
 		}
 	}
@@ -79,7 +73,7 @@ func TestConsumerPool_Duration(t *testing.T) {
 			break
 		}
 	}
-	if time.Now().Sub(start) > time.Second {
-		t.Fatal("too long time")
+	if time.Now().Sub(start) < time.Second {
+		t.Fatal("too short time")
 	}
 }
