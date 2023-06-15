ab819a902ae25eb52cf3946362e677075dfdee01
秦冲 <qinchong-cx@huafang.com>
2023-06-15 17:10:06 +0800

    fixed recovery

--- recovery/server.go
+++ recovery/server.go
@@ -1,24 +1,38 @@
 package recovery
 
 import (
+	"context"
+	"fmt"
 	"runtime/debug"
 
-	recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
-	return recovery.UnaryServerInterceptor(recovery.WithRecoveryHandler(func(p interface{}) error {
-		debug.PrintStack()
-		return status.Errorf(codes.Internal, "%v", p)
-	}))
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = toPanicError(r, req, info.FullMethod)
+			}
+		}()
+		return handler(ctx, req)
+	}
 }
 
 func StreamServerInterceptor() grpc.StreamServerInterceptor {
-	return recovery.StreamServerInterceptor(recovery.WithRecoveryHandler(func(p interface{}) error {
-		debug.PrintStack()
-		return status.Errorf(codes.Internal, "%v", p)
-	}))
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = toPanicError(r, "", info.FullMethod)
+			}
+		}()
+		return handler(srv, stream)
+	}
+}
+
+func toPanicError(r, req interface{}, m string) error {
+	fmt.Printf("path: %s\t request:%+v\tpanic:%+v\n%s", m, req, r, debug.Stack())
+	return status.Errorf(codes.Internal, "%v", r)
 }
