5e1d9682445a5fb860d6432d311765609455df1b
Yuriy <youlu@outlook.com>
2023-07-18 16:27:46 +0800

    Add: custom outgoing metadata

--- metadata/server.go
+++ metadata/server.go
@@ -35,6 +35,78 @@ func (s *ctxWrapper) Context() context.Context {
 	return context.WithValue(ctx, metadataKey{}, md)
 }
 
+func AppendOutgoingMetadata(ctx context.Context, md *common.Metadata) context.Context {
+	if md == nil {
+		return context.WithValue(ctx, metadataKey{}, &common.Metadata{})
+	}
+	oldMD := IncomingMetadata(ctx)
+	if oldMD == nil {
+		return context.WithValue(ctx, metadataKey{}, md)
+	}
+	//
+	if md.GetPlatform() > 0 {
+		oldMD.Platform = md.Platform
+	}
+	if md.GetNetwork() > 0 {
+		oldMD.Network = md.Network
+	}
+	if md.GetPackage() != "" {
+		oldMD.Package = md.Package
+	}
+	if md.GetVersion() != "" {
+		oldMD.Version = md.Version
+	}
+	if md.GetOsVersion() != "" {
+		oldMD.OsVersion = md.OsVersion
+	}
+	if md.GetBrand() != "" {
+		oldMD.Brand = md.Brand
+	}
+	if md.GetModel() != "" {
+		oldMD.Model = md.Model
+	}
+	if md.GetDeviceId() != "" {
+		oldMD.DeviceId = md.DeviceId
+	}
+	if md.GetIsEmulator() {
+		oldMD.IsEmulator = md.IsEmulator
+	}
+	if md.GetIsDebug() {
+		oldMD.IsDebug = md.IsDebug
+	}
+	if md.GetClientIp() != "" {
+		oldMD.ClientIp = md.ClientIp
+	}
+	if md.GetChannel() != "" {
+		oldMD.Channel = md.Channel
+	}
+	if md.GetProduct() != "" {
+		oldMD.Product = md.Product
+	}
+	if md.GetTraceId() != "" {
+		oldMD.TraceId = md.TraceId
+	}
+	if md.GetRiskId() != "" {
+		oldMD.RiskId = md.RiskId
+	}
+	if md.GetUuid() != "" {
+		oldMD.Uuid = md.Uuid
+	}
+	if md.GetUdid() != "" {
+		oldMD.Udid = md.Udid
+	}
+	if md.GetUserAgent() != "" {
+		oldMD.UserAgent = md.UserAgent
+	}
+	if md.GetDeviceMac() != "" {
+		oldMD.DeviceMac = md.DeviceMac
+	}
+	if md.GetAndroidId() != "" {
+		oldMD.AndroidId = md.AndroidId
+	}
+	return context.WithValue(ctx, metadataKey{}, oldMD)
+}
+
 func ContextWithProduct(ctx context.Context, product string) context.Context {
 	return context.WithValue(ctx, metadataKey{}, &common.Metadata{
 		Product: product,
